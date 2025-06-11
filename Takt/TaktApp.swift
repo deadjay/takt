3c14ead2cd2384b48018b29d3c014f7f68909149
Artem Alekseev <iaffector@gmail.com>
2025-06-11 23:07:54 +0200

    Initial Commit

---
+++ Takt/TaktApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaktApp.swift
+//  Takt
+//
+//  Created by Artem Alekseev on 11.06.25.
+//
+
+import SwiftUI
+
+@main
+struct TaktApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
